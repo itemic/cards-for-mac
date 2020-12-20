9ed6db91b51aa88087a268533667842cdd1376d3
Terran Kroft <terrankroft@gmail.com>
2020-12-20 15:55:22 +0800

    Initial Commit

---
+++ Cards for Mac/Cards_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cards_for_MacApp.swift
+//  Cards for Mac
+//
+//  Created by Terran Kroft on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Cards_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
